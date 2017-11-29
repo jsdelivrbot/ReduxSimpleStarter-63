70043c61fca9f704870e2e1fc9d64fdadf92ba04
Chris Carr <christocarr@outlook.com>
2017-11-29 21:21:15 +0000

    state added to search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-const SearchBar = () => {
-	return <input />;
-};
+class SearchBar extends React.Component {
+	constructor(props) {
+		super(props);	
+		
+		this.state = { term: '' };
+	}
+	
+	render () {
+		return (
+			<div>
+				<input onChange={event => this.setState({ term: event.target.value})} />
+				
+			</div>
+		);
+	}
+	
+	//What is inside the curly braces in the render function //is the same as -
+	
+	//onInputChange(e) {
+	//	console.log(e.target.value);
+	//}
+}
 
 export default SearchBar;
