151f3b71b51f21456026d612c23eb92794dfaf5c
Chris Carr <christocarr@outlook.com>
2017-11-29 18:26:29 +0000

    API added and search bar component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAt4z7rxSmxphrHGtOu7yHLYxefrVFEqi8';
+
 //create a new component. This component should produce 
 //some HTML.
-const App = function() {
-	return <div>Hi!</div>;
+const App = () => {
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 
 //Take this component's generated HTML and put it on the //page(in the DOM).
+ReactDOM.render(<App />, document.querySelector('.container'));
