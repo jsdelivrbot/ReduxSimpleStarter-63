c78828c035366481af8aaca0a71de1ff8d479ae6
Chris Carr <christocarr@outlook.com>
2017-12-02 10:22:10 +0000

    YTsearch added to constructor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
@@ -7,12 +8,24 @@ const API_KEY = 'AIzaSyAt4z7rxSmxphrHGtOu7yHLYxefrVFEqi8';
 
 //create a new component. This component should produce 
 //some HTML.
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {videos: [] };
+		
+		YTSearch({key: API_KEY term: 'surfboards'}, (videos) => { 		this.setState({videos});
+			//this.setState( {videos: videos} );								
+		});
+	}
+	
+	render (){
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
 
 
